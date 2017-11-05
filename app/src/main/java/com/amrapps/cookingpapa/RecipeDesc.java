d8458685d17805561b5c9bc0230aa2b18403395a
Alex Reyes <reyesruizalex@gmail.com>
2017-11-04 23:54:25 -0400

    Made images work

--- app/src/main/java/com/amrapps/cookingpapa/RecipeDesc.java
+++ app/src/main/java/com/amrapps/cookingpapa/RecipeDesc.java
@@ -1,10 +1,13 @@
 package com.amrapps.cookingpapa;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -12,6 +15,8 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.io.InputStream;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Iterator;
 
@@ -29,19 +34,22 @@ public class RecipeDesc extends AppCompatActivity {
         final ImageView theImage = findViewById(R.id.image);
         final TextView theDesc = findViewById(R.id.description);
 
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+
+        StrictMode.setThreadPolicy(policy);
+
         // Read from the database
         myRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 // This method is called once with the initial value and again
                 // whenever data at this location is updated.
-                String image = dataSnapshot.child("Cookies").child("img").getValue(String.class);
-
-                String desc = dataSnapshot.child("Cookies").child("3").getValue(String.class);
-                theDesc.setText(desc);
+                String imageURL = dataSnapshot.child("Cookies").child("img").getValue(String.class);
+                Toast.makeText(getApplicationContext(), imageURL, Toast.LENGTH_SHORT).show();
+                Log.d("url", imageURL);
+                theImage.setImageDrawable(LoadImageFromWebOperations(imageURL));
 
                 ArrayList<String> RecipeThings = new ArrayList<>();
-
 
                 String holder = getIntent().getStringExtra("Holder");
 
@@ -65,6 +73,17 @@ public class RecipeDesc extends AppCompatActivity {
                 Log.w("RecipeDesc", "Failed to read value.", error.toException());
             }
         });
+    }
+    public static Drawable LoadImageFromWebOperations(String url) {
+        try {
+            Log.d("loadImage", "inside loadImage method");
+            InputStream is = (InputStream) new URL(url).getContent();
+            Drawable d = Drawable.createFromStream(is, "src name");
+            return d;
+        } catch (Exception e) {
+            Log.d("Exception", "" + e);
+            return null;
+        }
     }
 
 }
--- app/src/main/java/com/amrapps/cookingpapa/bar.java
+++ app/src/main/java/com/amrapps/cookingpapa/bar.java
@@ -1,15 +1,11 @@
 package com.amrapps.cookingpapa;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.TextView;
-import android.content.Intent;
 
 public class bar extends AppCompatActivity {
 
@@ -26,8 +22,6 @@ public class bar extends AppCompatActivity {
             @Override
             public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
                 System.out.println(seekBar.getProgress());
-
-
 
                 int x = seekBar.getProgress();
                 String[] vals = {"1","2","3","4","5","6","7","8","9","10"};
@@ -59,7 +53,7 @@ public class bar extends AppCompatActivity {
     public void clicked(View view){
         Intent value_of_slider = new Intent(getBaseContext(), MainActivity.class);
         value_of_slider.putExtra("slider_value", tv.getText());
-        System.out.println("This is yoour value: " + value_of_slider.getStringExtra("slider_value"));
+        System.out.println("This is your value: " + value_of_slider.getStringExtra("slider_value"));
         startActivity(value_of_slider);
     }
 
