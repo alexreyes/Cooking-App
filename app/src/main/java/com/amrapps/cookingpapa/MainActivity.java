1d8a0b261ab478634f4728c619adae182e5312fd
Alex Reyes <reyesruizalex@gmail.com>
2017-11-04 20:40:14 -0400

    Add recipe page

--- app/src/main/java/com/amrapps/cookingpapa/MainActivity.java
+++ app/src/main/java/com/amrapps/cookingpapa/MainActivity.java
@@ -1,7 +1,10 @@
 package com.amrapps.cookingpapa;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button activityRecipe = (Button) findViewById(R.id.activityRecipe);
+        final Intent intent = new Intent(this, RecipeDesc.class);
+
+        activityRecipe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(intent);
+            }
+        });
     }
+
 }
