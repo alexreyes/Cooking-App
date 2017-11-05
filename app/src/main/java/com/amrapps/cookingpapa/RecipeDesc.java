85fb7f5e2fbb04e3806422b2510f028298009e07
Alex Reyes <reyesruizalex@gmail.com>
2017-11-04 22:18:07 -0400

    before pull

--- app/src/main/java/com/amrapps/cookingpapa/RecipeDesc.java
+++ app/src/main/java/com/amrapps/cookingpapa/RecipeDesc.java
@@ -4,12 +4,17 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Iterator;
 
 public class RecipeDesc extends AppCompatActivity {
 
@@ -23,14 +28,36 @@ public class RecipeDesc extends AppCompatActivity {
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference("Recipes");
 
+        final TextView theTitle = findViewById(R.id.title);
+        final ImageView theImage = findViewById(R.id.image);
+        final TextView theDesc = findViewById(R.id.description);
+
         // Read from the database
         myRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 // This method is called once with the initial value and again
                 // whenever data at this location is updated.
-                String value = dataSnapshot.getValue(String.class);
-                Log.d("RecipeDesc", "Value is: " + value);
+                String title = dataSnapshot.child("Cookies").child("name").getValue(String.class);
+                theTitle.setText(title);
+
+                String image = dataSnapshot.child("Cookies").child("img").getValue(String.class);
+
+                String desc = dataSnapshot.child("Cookies").child("3").getValue(String.class);
+                theDesc.setText(desc);
+
+                ArrayList<String> RecipeThings = new ArrayList<>();
+
+                Iterator<DataSnapshot> items = dataSnapshot.getChildren().iterator();
+                RecipeThings.clear();
+                while(items.hasNext()){
+                    DataSnapshot item = items.next();
+                    String recipe = item.child("name").getValue(String.class);
+
+                    RecipeThings.add(recipe);
+                }
+                theTitle.setText(items + "");
+                Log.d("RecipeThings", RecipeThings + "");
             }
 
             @Override
