2f53aaf949ffaca913670e0d99120cda1eb4fed3
Dylan Kane <dyl@Dylans-MacBook-Pro.local>
2017-11-05 01:16:43 -0400

    second listview

--- app/src/main/java/com/amrapps/cookingpapa/RecipeDesc.java
+++ app/src/main/java/com/amrapps/cookingpapa/RecipeDesc.java
@@ -1,10 +1,15 @@
 package com.amrapps.cookingpapa;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
@@ -15,8 +20,11 @@ import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 public class RecipeDesc extends AppCompatActivity {
+
+    private ListView ingredientList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +36,8 @@ public class RecipeDesc extends AppCompatActivity {
 
         final TextView theTitle = findViewById(R.id.title);
         final ImageView theImage = findViewById(R.id.image);
-        final TextView theDesc = findViewById(R.id.description);
+        ingredientList = findViewById(R.id.ingredientsList);
+        //final TextView theDesc = findViewById(R.id.description);
 
         // Read from the database
         myRef.addValueEventListener(new ValueEventListener() {
@@ -38,19 +47,20 @@ public class RecipeDesc extends AppCompatActivity {
                 // whenever data at this location is updated.
                 String image = dataSnapshot.child("Cookies").child("img").getValue(String.class);
 
-                String desc = dataSnapshot.child("Cookies").child("3").getValue(String.class);
-                theDesc.setText(desc);
+                //String desc = dataSnapshot.child("Cookies").child("3").getValue(String.class);
+                //theDesc.setText(desc);
 
                 ArrayList<String> RecipeThings = new ArrayList<>();
 
-
                 String holder = getIntent().getStringExtra("Holder");
+                showData(dataSnapshot, holder);
+
 
                 Iterator<DataSnapshot> items = dataSnapshot.child(holder).child("Ingredients").getChildren().iterator();
                 RecipeThings.clear();
                 while(items.hasNext()){
                     DataSnapshot item = items.next();
-                    String recipe = item.getValue(String.class);
+                    String recipe = item.toString();
 
                     RecipeThings.add(recipe);
                 }
@@ -66,6 +76,29 @@ public class RecipeDesc extends AppCompatActivity {
                 Log.w("RecipeDesc", "Failed to read value.", error.toException());
             }
         });
+    }
+
+    public void showData(DataSnapshot dataSnapshot, String holder){
+        ArrayList<indiIngredients> IngredientThings = new ArrayList<>();
+
+        Iterator<DataSnapshot> items = dataSnapshot.child(holder).child("Ingredients").getChildren().iterator();
+        IngredientThings.clear();
+        while(items.hasNext()){
+            indiIngredients ingredient = new indiIngredients();
+            DataSnapshot item = items.next();
+            ingredient.setIngredientName(item.getValue(String.class));
+
+            IngredientThings.add(ingredient);
+        }
+        Log.d("he", "Hello Here" + IngredientThings.size());
+        ArrayAdapter adapter = new customListAdapterIngredients(this, IngredientThings);
+        ingredientList.setAdapter(adapter);
+
+    }
+
+    public void ingredientAction(View v){
+
+
     }
 
 }
